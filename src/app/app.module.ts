a187cfef8e65cf7124d4e4e5390abef8e666345b
quajoduke <quajoduke305@gmail.com>
2019-11-05 11:08:45 +0000

    product track listing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { ProductDescriptionComponent } from './product-description/product-description.component';
-import { ProductService } from './product.service';
+import {AppComponent} from './app.component';
+import {ProductPageComponent} from './product-page/product-page.component';
+import {ProductDescriptionComponent} from './product-description/product-description.component';
+import {ProductTracklistingComponent} from './product-tracklisting/product-tracklisting.component';
+import {ProductService} from './product.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductTracklistingComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
