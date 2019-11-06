fda6f14acab7efedaca8be8de2501acff7a99df1
quajoduke <quajoduke305@gmail.com>
2019-11-06 12:56:28 +0000

    add route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {ProductPageComponent} from './product-page/product-page.component';
 import {ProductDescriptionComponent} from './product-description/product-description.component';
 import {ProductTracklistingComponent} from './product-tracklisting/product-tracklisting.component';
 import {ProductService} from './product.service';
-import { ProductListComponent } from './product-list/product-list.component';
+import {ProductListComponent} from './product-list/product-list.component';
 
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'products', pathMatch: 'full'},
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductPageComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +24,12 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductTracklistingComponent,
     ProductListComponent
   ],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
